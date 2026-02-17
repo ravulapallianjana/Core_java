51e546db9ed978ae3b85b9ca63fddca42adbd574
Ravulapalli Anjana <ravulapallianjana2005@gmail.com>
2026-02-17 11:34:53 +0530

    USING DATA TYPES

---
+++ repo/src/repo/Primitive.java
@@ -0,0 +1,18 @@
+package repo;
+import java.math.BigInteger;
+
+
+public class Primitive {
+BigInteger b1=new BigInteger("100") ;
+BigInteger b2=new BigInteger("1000");
+
+	public static void main(String[] args) {
+		Primitive pr=new Primitive();
+		System.out.println(pr.b1. add (pr.b2));
+		System.out.println(pr.b1. multiply (pr.b2));
+		System.out.println(pr.b1. divide (pr.b2));
+		
+		
+	}
+
+}
---
+++ repo/src/repo/TestDemo1.java
@@ -0,0 +1,26 @@
+package repo;
+
+public class TestDemo1 {
+	int bookid;
+	String title;
+	String author;
+	 static double price;
+	static double discount;
+
+	public static void main(String[] args) {
+		TestDemo1 td=new TestDemo1();
+		td.bookid=1;
+td.title="java";
+td.author="ANJANA";
+price=200;
+discount=0.1;
+ double total=price-discount;
+ System.out.println("author="+ "ANJANA");
+ System.out.println("title="+ "java") ;
+System.out.println("total="+(price-discount) );
+System.out.println("totalprice="+(price*discount) );
+
+	}
+	
+
+}
